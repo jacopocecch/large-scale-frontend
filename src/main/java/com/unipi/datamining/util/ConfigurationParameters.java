b2fe747f8f122852b7e3c52f40dab6741a479bb7
Jacopo Cecchetti <j.cecchetti23@gmail.com>
2022-01-19 12:53:03 +0100

    first commit

---
+++ src/main/java/com/unipi/datamining/util/ConfigurationParameters.java
@@ -0,0 +1,31 @@
+package com.unipi.datamining.util;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.security.AnyTypePermission;
+import com.unipi.datamining.PersonalityClustering;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@XStreamAlias("ConfigurationParameters")
+public class ConfigurationParameters {
+    public String serverIP;
+    public int serverPort;
+
+    public ConfigurationParameters(String fileXML){
+        try {
+            System.out.println("Reading the configuration parameters..");
+            XStream xstream = new XStream();
+            xstream.alias("ConfigurationParameters", ConfigurationParameters.class);
+            xstream.addPermission(AnyTypePermission.ANY);
+            xstream.processAnnotations(ConfigurationParameters.class);
+            ConfigurationParameters p = (ConfigurationParameters)(xstream.fromXML(new String(Files.readAllBytes(Paths.get(fileXML)))));
+            serverIP = p.serverIP;
+            serverPort = p.serverPort;
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
